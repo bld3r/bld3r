cb884f990b276d046353b0afdd47e2290e2dd25f
bld3r <bld3r.com@gmail.com>
2014-03-28 18:49:32 -0700

    masonry updates

--- js/masonry_infinite.js
+++ js/masonry_infinite.js
@@ -1,21 +1,43 @@
-$(document).ready(function(){
-  $('.long_load').animate({"opacity": "1"}, 10000);
+$(document).ready(function(){      
+  //$('.long_load').animate({"opacity": "1"}, 10000);
   var $container = $('#masonry_division');
   $container.imagesLoaded(function(){
     $('.loader').fadeOut("fast");
+    $container.fadeTo("default_duration", 1);
       $container.masonry({
-      itemSelector : '.masonry_object',
-            animate: true,
-            isFitWidth: true
-      }).fadeTo("default_duration",1);
-      $(".background").fadeTo("default_duration", 1);
+        itemSelector : '.masonry_object',
+        isFitWidth: true,
+        transitionDuration: 0
+      });
+    $('.masonry_object img.lazy').lazyload({
+        threshold : 10,
+        effect: 'fadeIn',
+        effectspeed: 1000,
+        load: function() {
+            // Disable trigger on this image
+            $(this).removeClass("lazy");
+            $container.masonry();
+        }
+    });
+    //$('.masonry_object img.lazy').trigger('scroll'); 
+      // // Custom fading animation (This may not be necessary)
+      // $('.masonry_object img').on('load', function() {
+      //   $(this).fadeIn(250);
+      // }).each(function() {
+      //   if(this.complete) {
+      //     $(this).load();
+      //   }
+      // });    
   });
+
+
 
   // Infinite Scroll
   $container.infinitescroll({
     debug: false,
     extraScrollPx: 0,
-    bufferPx: 0,
+    bufferPx: 40,
+    pixelsFromNavToBottom:10,
     navSelector  : '#navigation',    // selector for the paged navigation 
     nextSelector : '#navigation a',  // selector for the NEXT link (to page 2)
     itemSelector : '.masonry_object',     // selector for all items you'll retrieve
@@ -30,13 +52,32 @@ $(document).ready(function(){
     // trigger Masonry as a callback
     function( newElements ) {
       // hide new items while they are loading
-      var $newElems = $( newElements ).css({display:"none"},{ opacity: 0 });
+      var $newElems = $( newElements );
       // ensure that images load before adding to masonry layout
       $newElems.imagesLoaded(function(){
         // show elems now they're ready
         
         $container.masonry( 'appended', $newElems, true ); 
-        $newElems.show().delay(1000).animate({ opacity: 1 },1000);
+        $('.masonry_object img.lazy').lazyload({
+            effect: 'fadeIn',
+            effectspeed: 1500 ,
+            load: function() {
+                // Disable trigger on this image
+                $(this).removeClass("lazy");
+                $container.masonry();
+            }
+        });
+        //$('.masonry_object img.lazy').trigger('scroll');         
+        
+        // // Custom fading animation (This may not be necessary)
+        // $newElems.on('load', function() {
+        //   $(this).fadeIn(250);
+        // }).each(function() {
+        //   if(this.complete) {
+        //     $(this).load();
+        //   }
+        // });
+
       });
     }
   );
